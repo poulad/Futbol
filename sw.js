4a15e43c9e4b1844839b0656e3cbc22ff24da898
Poulad <poulad.pld@gmail.com>
2018-06-24 16:36:52 -0400

    Build for github pages

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    console.log('installing sw...');
+});
+
+self.registration.showNotification('Hello from service worker', {
+    tag: 'sw',
+    body: 'at ' + new Date()
+});
