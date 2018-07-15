f2f4f46f131fbf8ddba25b7cbbb13cd4d3493c08
Poulad <poulad.pld@gmail.com>
2018-07-15 08:03:23 -0400

    Change VAPID key

--- src/ClientApp/src/environments/environment.prod.ts
+++ src/ClientApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiKey: '53b8cdf71c054628a6a57be9a927178c',
-    vapid: 'BLWrzdvn_n_4w_Z55WIEVYxgfmaOOvEVgvm90o9Vey0bhZB0QYGgibBRzppZCkr8VyzisnnUs5MW8g-F2ss-8oI',
+    vapid: 'BJpwmPJ14GR1wV_kU8Xuh3VP0gETWKX6RAMn0S5YGNUg6Wx5QUtHG4ZjNCAkaY0wG7wScZxrNXwk9XQuLeK46s8',
 };
